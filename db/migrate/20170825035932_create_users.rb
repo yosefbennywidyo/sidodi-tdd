d9e5876e25d43d83d309b584f55484de0f62d956
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 04:10:58 +0000

    Add a Join Table

--- db/migrate/20170825035932_create_users.rb
+++ db/migrate/20170825035932_create_users.rb
@@ -5,8 +5,17 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :last_name
       t.string :email
       t.string :password_digest #for storing crypted passwords
-
-      t.timestamps
+      t.timestamps #default created_at and updated_at attributes
     end
+      
+      #adding a join table for has_and_belongs_to_many relationship
+      #model order needs to be alphabetical
+    create_table :sections_users do |t|
+      t.integer :section_id
+      t.integer :user_id 
+    end
+    
+    #add an index to speed queries up
+    add_index(:sections_users, [:section_id, :user_id])
   end
 end
