6871f785a516da29ea9aae93ac4f0ddf85d358da
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 05:08:25 +0000

    Add Comment MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :statuses
   resources :roles
   resources :incoming_mails
---
+++ spec/factories/comments.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :comment do
+    body "MyText"
+  end
+end
