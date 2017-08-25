ffe0c5305cdf26703bb9cb18a1100f86a1f5a7fc
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 04:02:30 +0000

    Add email and password_digest

--- db/migrate/20170825035932_create_users.rb
+++ db/migrate/20170825035932_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.string :password_digest #for storing crypted passwords
 
       t.timestamps
     end
