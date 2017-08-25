0600da1f14d03457384b18d636d7e671091da60d
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 05:01:57 +0000

    Add Role to User

--- db/migrate/20170825035932_create_users.rb
+++ db/migrate/20170825035932_create_users.rb
@@ -12,10 +12,11 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       #model order needs to be alphabetical
     create_table :sections_users do |t|
       t.integer :section_id
-      t.integer :user_id 
+      t.integer :user_id
+      t.integer :role_id
     end
     
     #add an index to speed queries up
-    add_index(:sections_users, [:section_id, :user_id])
+    add_index(:sections_users, [:section_id, :user_id, :role_id])
   end
 end
