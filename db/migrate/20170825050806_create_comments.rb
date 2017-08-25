ca0ba5d787b8b623299e6dac72f75e4d9f91dc23
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 05:33:47 +0000

    Add references to Comment table

--- db/migrate/20170825050806_create_comments.rb
+++ db/migrate/20170825050806_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.text :body
+      t.references :commentable, :polymorphic => true 
+      t.integer :user_id
 
       t.timestamps
     end
