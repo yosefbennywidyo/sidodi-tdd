044cff7b02f89f7affa42e892b297ac7706dbb0e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-25 04:19:04 +0000

    Add table link

--- db/migrate/20170825041259_create_outgoing_mails.rb
+++ db/migrate/20170825041259_create_outgoing_mails.rb
@@ -3,8 +3,14 @@ class CreateOutgoingMails < ActiveRecord::Migration[5.1]
     create_table :outgoing_mails do |t|
       t.string :title
       t.string :description
+      t.integer :section_id #Link to section
+      t.integer :user_id #Link to user who create it
 
       t.timestamps
     end
+    #add an index to speed queries up
+    add_index :outgoing_mails, :section_id
+    #add an index to speed queries up
+    add_index :outgoing_mails, :user_id
   end
 end
